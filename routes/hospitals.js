3513be7a3c488a25657d686240119d864fbd3287
Thanat Phrudprisan <thanat.phr@gmail.com>
2023-02-19 19:56:16 +0700

    Assignment 6

--- routes/hospitals.js
+++ routes/hospitals.js
@@ -2,9 +2,9 @@ const express = require('express')
 const { getHospitals, createHospital, getHospital, deleteHospital, updateHospital } = require('../controller/hospitals')
 const router = express.Router()
 
-const app = express()
+const { protect, authorize } = require('../middleware/auth')
 
-router.route('/').get(getHospitals).post(createHospital)
-router.route('/:id').get(getHospital).delete(deleteHospital).put(updateHospital)
+router.route('/').get(getHospitals).post(protect, authorize('admin'), createHospital)
+router.route('/:id').get(getHospital).delete(protect, authorize('admin'), deleteHospital).put(protect, authorize('admin'), updateHospital)
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const cookieParser = require('cookie-parser')
 const connectDB = require('./config/db')
 
 const hospitals = require('./routes/hospitals')
+const auth = require('./routes/auth')
 
 dotenv.config({path: './config/config.env'})
 connectDB()
@@ -10,7 +12,10 @@ connectDB()
 const app = express()
 
 app.use(express.json())
+app.use(cookieParser())
+
 app.use('/api/v1/hospitals', hospitals)
+app.use('/api/v1/auth', auth)
 
 const PORT = process.env.PORT || 3333
 
